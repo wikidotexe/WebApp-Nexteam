2e75582e61e04da3c6dd8592e9c46fc56eb580ba
wikiarlnm <07wikiarlianm@gmail.com>
2024-11-21 17:06:53 +0700

    ganti title body

--- resources/views/livewire/show-home.blade.php
+++ resources/views/livewire/show-home.blade.php
@@ -4,7 +4,7 @@
     <div class="row align-items-center justify-content-center">
       <div class="col-lg-6 mb-5 mb-lg-0">
         <div class="block text-center text-lg-start pe-0 pe-xl-5">
-          <h1 class="text-capitalize mb-4">Innovate. Excel. Succeed!</h1>
+          <h1 class="text-capitalize mb-4">Innovate. Excellent!. Succeed!</h1>
           <p class="mb-4">Computers & Internet Website.</p> <a type="button"
             class="btn btn-primary" href="#" data-bs-toggle="modal" data-bs-target="#applyLoan">See More<span style="font-size: 14px;" class="ms-2 fas fa-arrow-right"></span></a>
         </div>
